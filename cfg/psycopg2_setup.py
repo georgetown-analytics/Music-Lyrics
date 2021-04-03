68727ec603a13988cb69df8ccbb61a8ec36e6bf9
aww195 <aww45@georgetown.edu>
2021-04-03 16:31:54 -0400

    Tweaks to cfg

--- cfg/psycopg2_setup.py
+++ cfg/psycopg2_setup.py
@@ -1,6 +1,6 @@
 import psycopg2 as ps
 # define credentials 
-​
+
 credentials = {'POSTGRES_ADDRESS' : 'music-lyrics.cgsxezi8cfhr.us-east-2.rds.amazonaws.com', # change to your endpoint
                'POSTGRES_PORT' : '5432', # change to your port
                'POSTGRES_USERNAME' : 'tool', # change to your username
@@ -12,6 +12,6 @@ conn = ps.connect(host=credentials['POSTGRES_ADDRESS'],
                   user=credentials['POSTGRES_USERNAME'],
                   password=credentials['POSTGRES_PASSWORD'],
                   port=credentials['POSTGRES_PORT'])
-​
+
 cur = conn.cursor()
-​
+
